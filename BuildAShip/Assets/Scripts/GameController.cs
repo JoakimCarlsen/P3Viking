b7935a9a0a0c19e633b19f8b64a4f72693e6384d
JoakimCarlsen <Joakim@h497.aau1x-2.klient.sydhavn.site.aau.dk>
2016-12-01 15:22:53 +0100

    Plank can now move all the way to leap motion

--- BuildAShip/Assets/Scripts/GameController.cs
+++ BuildAShip/Assets/Scripts/GameController.cs
@@ -7,9 +7,8 @@ public class GameController : MonoBehaviour {
 	public GameObject Plank;
 	public GameObject Group1;
 
-	public Vector3 plankPos;
-
-	bool plankFree = false;
+	bool plankFire = false;
+	bool plankPlace = false;
 
 	public ChopWood chopWoodScript;
 
@@ -24,26 +23,42 @@ public class GameController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (chopWoodScript.hitCount == 5) {
+		if (chopWoodScript.hitCount == 2) {
 //			Log.gameObject.SetActive (false);
 			Plank.gameObject.SetActive (true);
 			StartCoroutine(plankWait());
 
 		}
 
-		if (plankFree == true){
+		if (plankFire == true){
 			
-
-			Plank.transform.position = plankPos;
+			Vector3 newPosition = new Vector3(-5f, 0.5f, 10f);
 
 			Quaternion newRotation = Quaternion.identity;
 			newRotation.eulerAngles = new Vector3(0f, 0f, 75f);
-			Plank.transform.rotation = Quaternion.Lerp(Plank.transform.rotation , newRotation , Time.deltaTime*4f);
+			Plank.transform.rotation = Quaternion.Lerp(Plank.transform.rotation , newRotation , Time.deltaTime*0.5f);
+
+			Plank.transform.position = Vector3.Lerp(Plank.transform.position , newPosition , Time.deltaTime*0.5f);
+		}
+
+		if (plankPlace == true){
+			Vector3 newPosition = new Vector3(20f, -0.5f, -3.61f);
+
+			Quaternion newRotation = Quaternion.identity;
+			newRotation.eulerAngles = new Vector3(0f, 90f, 100f);
+			Plank.transform.rotation = Quaternion.Lerp(Plank.transform.rotation , newRotation , Time.deltaTime*0.5f);
+
+			Plank.transform.position = Vector3.Lerp(Plank.transform.position , newPosition , Time.deltaTime*0.5f);
 		}
 	}
 
 	IEnumerator plankWait() {
 		yield return new WaitForSeconds(4);
-		plankFree = true;
+		plankFire = true;
+
+		yield return new WaitForSeconds (10);
+		plankFire = false;
+		plankPlace = true;
+
 	}
 }
