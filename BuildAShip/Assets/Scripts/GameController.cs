4c690d5cd52020179cc861a709e16d69ccab8ead
JoakimCarlsen <Joakim@h497.aau1x-2.klient.sydhavn.site.aau.dk>
2016-12-02 15:46:47 +0100

    Plank changes to new object over fire and resets

--- BuildAShip/Assets/Scripts/GameController.cs
+++ BuildAShip/Assets/Scripts/GameController.cs
@@ -6,9 +6,12 @@ public class GameController : MonoBehaviour {
 	public GameObject Log;
 	public GameObject Plank;
 	public GameObject Group1;
+	public GameObject part;
 
 	bool plankFire = false;
-	bool plankPlace = false;
+	bool partPlace = false;
+
+	bool partIsThere = false;
 
 	public ChopWood chopWoodScript;
 
@@ -23,13 +26,13 @@ public class GameController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (chopWoodScript.hitCount == 2) {
+		if (chopWoodScript.hitCount == 3) {
 //			Log.gameObject.SetActive (false);
 			Plank.gameObject.SetActive (true);
 			StartCoroutine(plankWait());
 
 		}
-
+		 // move plank to fire
 		if (plankFire == true){
 			
 			Vector3 newPosition = new Vector3(-5f, 0.5f, 10f);
@@ -41,14 +44,33 @@ public class GameController : MonoBehaviour {
 			Plank.transform.position = Vector3.Lerp(Plank.transform.position , newPosition , Time.deltaTime*0.5f);
 		}
 
-		if (plankPlace == true){
+		if (plankFire == false){
+
+			Vector3 newPosition = new Vector3(-20.2f, -0.42f, -3.61f);
+
+			Quaternion newRotation = Quaternion.identity;
+			newRotation.eulerAngles = new Vector3(90f, 0f, 0f);
+			Plank.transform.rotation = newRotation;
+
+			//Plank.transform.rotation = Quaternion.Lerp(Plank.transform.rotation , newRotation , Time.deltaTime*0.5f);
+
+			Plank.transform.position = newPosition;
+			//Plank.transform.position = Vector3(Plank.transform.position , newPosition , Time.deltaTime*0.5f);
+		}
+
+		// Move plank to leap motion
+		if (partPlace == true){
 			Vector3 newPosition = new Vector3(20f, -0.5f, -3.61f);
 
 			Quaternion newRotation = Quaternion.identity;
-			newRotation.eulerAngles = new Vector3(0f, 90f, 100f);
-			Plank.transform.rotation = Quaternion.Lerp(Plank.transform.rotation , newRotation , Time.deltaTime*0.5f);
+			newRotation.eulerAngles = new Vector3(0f, 90f, 90f);
+			part.transform.rotation = Quaternion.Lerp(part.transform.rotation , newRotation , Time.deltaTime*0.5f);
 
-			Plank.transform.position = Vector3.Lerp(Plank.transform.position , newPosition , Time.deltaTime*0.5f);
+			part.transform.position = Vector3.Lerp(part.transform.position , newPosition , Time.deltaTime*0.5f);
+		}
+
+		if (partIsThere == true){
+			part.SetActive(true);
 		}
 	}
 
@@ -56,11 +78,18 @@ public class GameController : MonoBehaviour {
 		yield return new WaitForSeconds(4);
 		plankFire = true;
 
-		yield return new WaitForSeconds (10);
+		yield return new WaitForSeconds (8);
 		plankFire = false;
-		plankPlace = true;
+		//Plank.SetActive(false);
+		partIsThere = true;
+
+		yield return new WaitForSeconds (1);
+		partPlace = true;
+
+		chopWoodScript.hitCount = 0;
+
 		yield return new WaitForSeconds (10);
-		plankPlace = false;
+		partPlace = false;
 
 	}
 }
